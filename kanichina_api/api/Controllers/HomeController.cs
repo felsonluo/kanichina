0a711d38041bdc723b5b5aa1ec3cc2b508242838
felsonluo <344913393@qq.com>
2018-08-07 22:41:05 +0800

    api

---
+++ kanichina_api/api/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace api.Controllers
+{
+    public class HomeController : Controller
+    {
+        public JsonResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return Json("");
+        }
+
+        public JsonResult GetProduct()
+        {
+            return Json(new Product() { id = "1" }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
